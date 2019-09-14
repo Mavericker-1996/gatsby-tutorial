19ed0d3bbbc51be8af8e1ca532189cea98d7ae10
佐七 <zuoqi.zzq@alibaba-inc.com>
2019-09-15 03:24:59 +0800

    gatsby tutorial chapter 02

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import containerStyles from "../styles/container.module.scss";
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+console.log(containerStyles);
+
+export default () => (
+  <div className={containerStyles.container}>
+    <Link to="/about/">About</Link>
+    <br />
+    <Link to="/user/">user</Link>
+    <Header headerText="Hello Gatsby!" />
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
