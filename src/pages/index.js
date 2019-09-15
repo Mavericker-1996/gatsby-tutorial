d7fefbac29f5870e909bee0389d7c2362d220053
mavericker-1996 <1398772969@qq.com>
2019-09-15 23:09:36 +0800

    chapter06: use transformer plugins to transfer markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import containerStyles from "../styles/container.module.scss"
 import Header from "../components/header"
+import Container from "../components/container"
 
 console.log(containerStyles)
 
-export default () => {
+export default ({ data }) => {
+  console.log(data)
   return (
-    <div className={containerStyles.container}>
-      <Link to="/about/">About</Link>
-      <br />
-      <Link to="/user/">user</Link>
-      <br />
-      <Link to="/my-files/">my-files</Link>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
+    <Container>
+      <div className={containerStyles.container}>
+        <Link to="/about/">About</Link>
+        <br />
+        <Link to="/user/">user</Link>
+        <br />
+        <Link to="/my-files/">my-files</Link>
+        <Header headerText="Hello Gatsby!" />
+      </div>
+
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title} <span style={{color: '#bbb'}}>- {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </Container>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
